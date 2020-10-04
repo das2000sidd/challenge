8468e706980f2d5c03c41e35bc8aae1b87ed6585
siddhartha 'sid' das <das2000sidd@gmail.com>
2020-10-04 11:05:44 -0500

    Delete Question_7.sh

--- Question_7.sh
+++
@@ -1,7 +0,0 @@
-cat farm-snapshot.txt | awk '{print $2}' | sort | uniq -c | sort -k1,1nr head -5 > top_5_users.txt
-
-cat farm-snapshot.txt | grep "pathpip" | wc -l
-
-grep "PEND" farm-snapshot.txt | wc -l
-
-grep "RUN" farm-snapshot.txt | wc -l
